a061802118dae92564c61669169ba01e555f05e1
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-10-07 23:05:09 +0800

    Setup Typescript project.

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+let productName: string = "Phone";
+
+interface Product {
+    id: number;
+    name: string;
+    isActive?: boolean;
+}
+
+const product: Product = {
+    id: 1,
+    name: "Phone"
+};
